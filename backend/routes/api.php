b5fa2a0b6d16f3a36b9a6dd8cd197432f23576b0
Jakub Klejczyk <jklejczyk@gmail.com>
2026-07-28 18:28:36 +0200

    fix: delete unused /user endpoint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,10 +11,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('v1/login', [AuthController::class, 'login'])->middleware('throttle:login');
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
 
